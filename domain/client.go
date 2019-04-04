945003cf1db66f12b964ddf87bffcc7d9f3b1a78
AFloresc <afloresc@edu.tecnocampus.cat>
2019-04-04 09:02:20 +0200

    Added domain elements

---
+++ domain/client.go
@@ -0,0 +1,61 @@
+package domain
+
+import (
+	"github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+// Client handles the db connection, performs basic operation to websays mysql tables
+type Client struct {
+	db *gorm.DB
+}
+
+// NewClient initialize a new db connection, needs a valid mysql.Config
+func NewClient(config *mysql.Config) (*Client, error) {
+	config.ParseTime = true
+	db, err := gorm.Open("mysql", config.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	client := Client{
+		db: db,
+	}
+	return &client, nil
+}
+
+// AutoMigrate run auto migration for given models, will only add missing fields, won't delete/change current data
+func (s *Client) AutoMigrate() error {
+
+	return s.db.AutoMigrate(
+		&Course{},
+		&Lesson{},
+		&Sublesson{}).Error
+}
+
+func (s *Client) Transaction() (*Client, error) {
+	client := Client{
+		db: s.db.Begin(),
+	}
+	return &client, nil
+}
+
+func NewTransaction(s *Client) (*Client, error) {
+	client := Client{
+		db: s.db.Begin(),
+	}
+	return &client, nil
+}
+
+func (s *Client) Rollback() {
+	s.db.Rollback()
+}
+
+func (s *Client) Commit() error {
+	return s.db.Commit().Error
+}
+
+// Close close current db connection.  If database connection is not an io.Closer, returns an error.
+func (s *Client) Close() error {
+	return s.db.Close()
+}
---
+++ domain/lesson.go
@@ -0,0 +1,58 @@
+package domain
+
+import (
+	"context"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+//Lesson represent every box inside the app
+type Lesson struct {
+	LessonID   int64          `json:"lessonID" gorm:"column:lessonID;primary_key"`
+	Title      string         `json:"titlee" gorm:"column:Title"`
+	Text       string         `json:"text" gorm:"column:Text;type:text"`
+	Image      string         `json:"image" gorm:"column:Image"`
+	UserID     int64          `json:"userID" gorm:"column:userID"`
+	CourseID   int64          `json:"courseID" gorm:"column:courseID"`
+	Sublessons []Sublesson    `json:"sublessons"`
+	CreatedAt  mysql.NullTime `json:"created" gorm:"column:created"`
+	UpdatedAt  mysql.NullTime `json:"updated" gorm:"column:updated"`
+}
+
+//TableName : table name for Gorm
+func (Lesson) TableName() string {
+	return "lessons"
+}
+
+// LessonClient : defines the interface to access Lesson data
+type LessonClient interface {
+	GetLesson(ctx context.Context, lessonID int64) (lesson *Lesson, err error)
+	CreateLesson(ctx context.Context, lesson *Lesson) error
+	UpdateLesson(ctx context.Context, lesson *Lesson) error
+	DeleteLesson(ctx context.Context, lessonID int64) error
+	GetAllSublessonsByCourse(ctx context.Context, courseid int64) ([]Lesson, error)
+}
+
+// asserts Client implements the LessonClient interface
+var _ LessonClient = (*Client)(nil)
+
+func (c Client) GetLesson(ctx context.Context, lessonID int64) (lesson *Lesson, err error) {
+	//TODO
+	return nil, nil
+}
+func (c Client) CreateLesson(ctx context.Context, lesson *Lesson) error {
+	//TODO
+	return nil
+}
+func (c Client) UpdateLesson(ctx context.Context, lesson *Lesson) error {
+	//TODO
+	return nil
+}
+func (c Client) DeleteLesson(ctx context.Context, lessonID int64) error {
+	//TODO
+	return nil
+}
+func (c Client) GetAllSublessonsByCourse(ctx context.Context, courseid int64) ([]Lesson, error) {
+	//TODO
+	return nil, nil
+}
