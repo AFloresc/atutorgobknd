c043e10abab4db1aa103edb34996308b5883d934
AFloresc <afloresc@edu.tecnocampus.cat>
2019-04-11 09:43:35 +0200

    modified main methods

--- domain/cmd/main.go
+++ domain/cmd/main.go
@@ -8,17 +8,11 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/atutor/ahttp"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 )
-
-//Book :
-type Book struct {
-	ID     int    `json:"id" gorm:"column:id;primary_key"`
-	Title  string `json:"title" gorm:"column:title;type:text"`
-	Author string `json:"author" gorm:"column:author;type:"`
-	Year   string `json:"yeah"`
-}
 
 // Application struct
 type Application struct {
@@ -38,13 +32,13 @@ type Config struct {
 	} `json:"database"`
 }
 
-var books []Book
+//var books []Book
 
 func main() {
-	app := Application{}
+	//app := Application{}
 
 	//client := MyClient{}
-	app.Initialize("config.json")
+	//app.Initialize("config.json")
 
 	// ctx := context.Background()
 
@@ -63,47 +57,33 @@ func main() {
 	// router.HandleFunc("/books", updateBook).Methods("PUT")
 	// router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
+	log.Println("Listening on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	log.Println("signup invoked.")
+
+	ahttp.RespondWithJSON(w, http.StatusOK, "succesfully called signup.")
 
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	log.Println("login invoked.")
+
+	ahttp.RespondWithJSON(w, http.StatusOK, "succesfully called login.")
 
 }
 
 func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
-	log.Println("protectedEndpoints invoked.")
+
+	ahttp.RespondWithJSON(w, http.StatusOK, "succesfully called protectedEndpoints.")
 
 }
 
 func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
 	log.Println("TokenVerifyMiddleWare invoked.")
+
 	return nil
 }
-
-// func getBooks(w http.ResponseWriter, r *http.Request) {
-// 	log.Println("Get all books is called.")
-// }
-// func getBook(w http.ResponseWriter, r *http.Request) {
-// 	log.Println("Get book is called.")
-// }
-
-// func addBook(w http.ResponseWriter, r *http.Request) {
-// 	log.Println("Add book is called.")
-// }
-
-// func updateBook(w http.ResponseWriter, r *http.Request) {
-// 	log.Println("Update book is called.")
-// }
-
-// func removeBook(w http.ResponseWriter, r *http.Request) {
-// 	log.Println("Remove book is called.")
-// }
 
 // Initialize : load data from json file
 func (a *Application) Initialize(filename string) {
@@ -142,110 +122,3 @@ func loadConfiguration(filename string) (Config, error) {
 	err = jsonParser.Decode(&config)
 	return config, err
 }
-
-// func timer(d time.Duration) <-chan int {
-// 	c := make(chan int)
-// 	go func() {
-// 		time.Sleep(d)
-// 		c <- 1
-// 		println("Timer: ", c)
-// 	}()
-// 	return c
-// }
-
-// func main() {
-// 	for i := 0; i < 24; i++ {
-// 		c := timer(1 * time.Second)
-// 		<-c
-// 	}
-// }
-
-// func main() {
-// 	// create new channel of type int
-// 	ch := make(chan int)
-
-// 	// start new anonymous goroutine
-// 	go func() {
-// 		// send 42 to channel
-// 		ch <- 42
-// 	}()
-// 	// read from channel
-// 	<-ch
-// }
-
-// var yeees = true
-// for {
-// fmt.Println("This will happen first")
-
-// go func() {
-// 	fmt.Println("This will happen at some unknown time")
-// }()
-
-// fmt.Println("This will either happen second or third")
-
-// fmt.Scanln()
-// fmt.Println("done")
-// if !yeees {
-// 	break
-// }
-
-// }
-
-//
-// 	var c chan string = make(chan string)
-
-// 	go pinger(c)
-// 	go ponger(c)
-// 	go printer(c)
-
-// 	var input string
-// 	fmt.Scanln(&input)
-// }
-// func pinger(c chan string) {
-// 	for i := 0; ; i++ {
-// 		c <- "ping"
-// 	}
-// }
-// func ponger(c chan string) {
-// 	for i := 0; ; i++ {
-// 		c <- " pong"
-// 	}
-// }
-// func printer(c chan string) {
-// 	for {
-// 		msg := <-c
-// 		fmt.Println(msg)
-// 		time.Sleep(time.Millisecond * 100)
-// 	}
-// }
-
-/*
-func pinger(c chan string) {
-    for i := 0; ; i++ {
-        c <- "ping"
-    }
-}
-func ponger(c chan string) {
-    for i := 0; ; i++ {
-        c <- "pong"
-    }
-}
-func printer(c chan string) {
-    for {
-        msg := <- c
-        fmt.Println(msg)
-        time.Sleep(time.Second * 1)
-    }
-}
-func main() {
-    var c chan string = make(chan string)
-
-    go pinger(c)
-    go ponger(c)
-    go printer(c)
-
-    var input string
-    fmt.Scanln(&input)
-}
-
-*/
