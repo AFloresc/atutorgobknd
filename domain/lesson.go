7ea5ef199ba7ab5e1179dfa6a13e5a218b1c2fec
AFloresc <afloresc@edu.tecnocampus.cat>
2019-05-18 11:23:24 +0200

    Implemented Lesson interface

--- domain/lesson.go
+++ domain/lesson.go
@@ -4,22 +4,24 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/atutor/utils"
 	"github.com/go-sql-driver/mysql"
 )
 
 //Lesson represent every box inside the app
 type Lesson struct {
-	LessonID  int64          `json:"lessonID" gorm:"column:lessonID;primary_key"`
-	Title     string         `json:"titlee" gorm:"column:Title"`
-	Text      string         `json:"text" gorm:"column:Text;type:text"`
-	Image     string         `json:"image" gorm:"column:Image"`
-	Language  string         `json:"language" gorm:"column:Language"`
-	Contents  []Content      `json:"contents"`
-	Test      Test           `json:"test"`
-	CreatedAt mysql.NullTime `json:"created" gorm:"column:created"`
-	UpdatedAt mysql.NullTime `json:"updated" gorm:"column:updated"`
-	DeletedAt mysql.NullTime `json:"deleted" gorm:"column:deleted"` //Soft delete feature
-	Position  int64          `json:"position" gorm:"column:position"`
+	LessonID    int64          `json:"lessonID" gorm:"column:lessonID;primary_key"`
+	Title       string         `json:"titlee" gorm:"column:Title"`
+	Text        string         `json:"text" gorm:"column:Text;type:text"`
+	Image       string         `json:"image" gorm:"column:Image;type:text"`
+	Language    string         `json:"language" gorm:"column:Language"`
+	Contents    []Content      `json:"contents"`
+	Questionary Questionary    `json:"questionary"`
+	CourseID    int64          `json:"courseID" gorm:"column:courseID"`
+	CreatedAt   mysql.NullTime `json:"created" gorm:"column:created"`
+	UpdatedAt   mysql.NullTime `json:"updated" gorm:"column:updated"`
+	DeletedAt   mysql.NullTime `json:"deleted" gorm:"column:deleted"` //Soft delete feature
+	Position    int64          `json:"position" gorm:"column:position"`
 }
 
 //TableName : table name for Gorm
@@ -29,8 +31,8 @@ func (l *Lesson) TableName() string {
 
 // LessonClient : defines the interface to access Lesson data
 type LessonClient interface {
-	GetLesson(ctx context.Context, lessonID int64) (lesson *Lesson, err error)
-	GetAllLessonsByCourseID(ctx context.Context, courseID int64, language string) (lesson []Lesson, err error)
+	GetLesson(ctx context.Context, lessonID int64) (lesson Lesson, err error)
+	GetAllLessonsByCourseID(ctx context.Context, courseID int64, language string) (lessons []Lesson, err error)
 	CreateLesson(ctx context.Context, lesson *Lesson) error
 	UpdateLesson(ctx context.Context, lesson *Lesson) error
 	DeleteLesson(ctx context.Context, lessonID int64) error
@@ -39,12 +41,30 @@ type LessonClient interface {
 // asserts Client implements the LessonClient interface
 var _ LessonClient = (*Client)(nil)
 
-func (c Client) GetLesson(ctx context.Context, lessonID int64) (lesson *Lesson, err error) {
-	//TODO
-	return nil, nil
+// GetLesson : retrieves a lesson by it's ID
+func (c Client) GetLesson(ctx context.Context, lessonID int64) (lesson Lesson, err error) {
+	ls := Lesson{}
+	err = c.db.Table("lessons").Where("lessonID = ?", lessonID).Find(&ls).Error
+	if err != nil {
+		fmt.Println(err)
+		return ls, nil
+	}
+	contents, err := c.GetAllContentByLessonID(ctx, ls.LessonID)
+	if err != nil {
+		fmt.Println(err)
+		return ls, nil
+	}
+	for _, content := range contents {
+		ls.Contents = append(ls.Contents, content)
+	}
+	return ls, nil
 }
 func (c Client) CreateLesson(ctx context.Context, lesson *Lesson) error {
-	//TODO
+	err := c.db.Create(&lesson).Error
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 func (c Client) UpdateLesson(ctx context.Context, lesson *Lesson) error {
@@ -66,7 +86,30 @@ func (c Client) DeleteLesson(ctx context.Context, lessonID int64) error {
 
 }
 
-func (c Client) GetAllLessonsByCourseID(ctx context.Context, courseID int64, language string) (lesson []Lesson, err error) {
-	//TODO
-	return nil, nil
+func (c Client) GetAllLessonsByCourseID(ctx context.Context, courseID int64, language string) (lessons []Lesson, err error) {
+
+	ls := []Lesson{}
+	err = c.db.Table("lessons").Where("courseID = ?", courseID).Find(&ls).Error
+	if err != nil {
+		fmt.Println(err)
+		return nil, nil
+	}
+	for index, lesson := range ls {
+		contents, err := c.GetAllContentByLessonID(ctx, lesson.LessonID)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		for _, content := range contents {
+			ls[index].Contents = append(ls[index].Contents, content)
+		}
+	}
+	return ls, nil
+}
+
+func (c Client) hardDeleteLesson(ctx context.Context, lessonID int64) error {
+	if lessonID != 0 {
+		return c.db.Exec("DELETE FROM lessons WHERE lessonID=? ", lessonID).Error
+	}
+	return utils.NewError("Lesson ID value not allowed on hard Delete action")
 }
--- domain/lesson_test.go
+++ domain/lesson_test.go
@@ -1,1 +1,184 @@
 package domain
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/atutor/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLesson(t *testing.T) {
+	dbConfig, err := utils.GetMySQLConfig()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	dbConfig.Passwd = "Bautista21"
+
+	dbConfig.Addr = "localhost:3306"
+	if err != nil {
+		return
+	}
+
+	dbConfig.DBName = "atutor_dev"
+
+	client, err := NewClient(dbConfig)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer client.Close()
+
+	err = client.AutoMigrate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mockLesson := Lesson{
+		CourseID: int64(1),
+		Title:    "Instalación de las herramientas necesarias",
+		Text:     "Descarga de las herramientas necesarias para seguir el curso.",
+		Language: "es",
+		Image:    "https://res.cloudinary.com/dnvu5jzwt/image/upload/v1558000026/c1l1_e9ojcw.png",
+		Position: 0,
+	}
+
+	mockLesson2 := Lesson{
+		CourseID: int64(1),
+		Title:    "El primer proyecto Android",
+		Text:     "Como crear nuestro primer proyecto Android.",
+		Language: "es",
+		Image:    "https://res.cloudinary.com/dnvu5jzwt/image/upload/v1558000089/project_snnkng.png",
+		Position: 1,
+	}
+
+	mockLesson3 := Lesson{
+		CourseID: int64(1),
+		Title:    "Capturar el click de un botón",
+		Text:     "Como utilizar buttons en Android",
+		Language: "es",
+		Image:    "https://res.cloudinary.com/dnvu5jzwt/image/upload/v1558000028/c1l2_dk87zd.png",
+		Position: 2,
+	}
+
+	ctx := context.Background()
+
+	t.Run("TestGetLessonNotExist", func(t *testing.T) {
+		_, err := client.GetLesson(ctx, mockLesson.LessonID)
+
+		assert := assert.New(t)
+
+		assert.Nil(err)
+
+	})
+
+	t.Run("TestCreateLesson", func(t *testing.T) {
+		err := client.CreateLesson(ctx, &mockLesson)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = client.CreateLesson(ctx, &mockLesson2)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = client.CreateLesson(ctx, &mockLesson3)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+	})
+
+	t.Run("TestGetLesson", func(t *testing.T) {
+		lesson, err := client.GetLesson(ctx, mockLesson.LessonID)
+
+		if err != nil {
+			t.Error(err)
+		}
+		assert := assert.New(t)
+
+		assert.Equal(mockLesson.LessonID, lesson.LessonID)
+	})
+
+	t.Run("TestUpdateLesson", func(t *testing.T) {
+		mockLesson.Title = "Preparación del entorno de desarollo"
+		err := client.UpdateLesson(ctx, &mockLesson)
+
+		if err != nil {
+			t.Error(err)
+		}
+		lesson, err := client.GetLesson(ctx, mockLesson.LessonID)
+
+		if err != nil {
+			t.Error(err)
+		}
+		assert := assert.New(t)
+
+		assert.Equal(mockLesson.Title, lesson.Title)
+	})
+
+	t.Run("TestDeleteLesson", func(t *testing.T) {
+		err := client.DeleteLesson(ctx, mockLesson.LessonID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = client.DeleteLesson(ctx, mockLesson2.LessonID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = client.DeleteLesson(ctx, mockLesson3.LessonID)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	// t.Run("TestHardDeleteCourse", func(t *testing.T) {
+	// 	err := client.hardDeleteLesson(ctx, mockLesson.LessonID)
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+
+	// 	err = client.hardDeleteLesson(ctx, mockLesson.LessonID)
+	// 	if err != nil {
+	// 		t.Fatal(err)
+	// 	}
+	// })
+}
+
+func TestGtLesson1(t *testing.T) {
+	dbConfig, err := utils.GetMySQLConfig()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	dbConfig.Passwd = "Bautista21"
+
+	dbConfig.Addr = "localhost:3306"
+	if err != nil {
+		return
+	}
+
+	dbConfig.DBName = "atutor_dev"
+
+	client, err := NewClient(dbConfig)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer client.Close()
+
+	err = client.AutoMigrate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lesson, err := client.GetLesson(context.Background(), int64(3))
+	if err != nil {
+		fmt.Println("Error retrieving lesson 3")
+	} else {
+		fmt.Println("LESSON---> ", lesson)
+	}
+
+}
