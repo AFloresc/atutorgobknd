ff62df2d94e79cc1beb94a305e88093d0077a981
AFloresc <afloresc@edu.tecnocampus.cat>
2019-06-10 20:27:06 +0200

    Added new endpoints and implementation

--- controller/application.go
+++ controller/application.go
@@ -25,18 +25,38 @@ type Application struct {
 	Client *domain.Client
 }
 
+// InitializeRoutes : defines de endpoints routes and initializes them
 func (ap *Application) InitializeRoutes(router *mux.Router) {
+	baseRoute := "/api/1.0/atapi"
+	fmt.Println(baseRoute + "/test")
 	//Signup handlers
-	router.HandleFunc("/signup", ap.signup).Methods("POST")
+	router.HandleFunc(baseRoute+"/signup", ap.signup).Methods("POST")
 	//Signin handers
-	router.HandleFunc("/login", ap.login).Methods("POST")
+	router.HandleFunc(baseRoute+"/login", ap.login).Methods("POST")
 	//token protected routes
-	router.HandleFunc("/protected", TokenVerifyMiddleWare(ap.protectedEndpoint)).Methods("GET")
-	router.HandleFunc("/test", TokenVerifyMiddleWare(TestEndpoint)).Methods("GET")
+	router.HandleFunc(baseRoute+"/protected", TokenVerifyMiddleWare(ap.protectedEndpoint)).Methods("GET")
+	router.HandleFunc(baseRoute+"/test", TokenVerifyMiddleWare(TestEndpoint)).Methods("GET")
 
 	//Concept routes
-	router.HandleFunc("/concepts/{conceptid}/language/{language}", ap.GetConcept).Methods("GET")
-	router.HandleFunc("/concepts/", ap.CreateConcept).Methods("POST")
+	router.HandleFunc(baseRoute+"/concepts/{conceptid}", ap.GetConcept).Methods("GET")
+	router.HandleFunc(baseRoute+"/concepts/languages/{language}", ap.GetAllConceptsByLanguage).Methods("GET")
+	router.HandleFunc(baseRoute+"/concepts/", ap.CreateConcept).Methods("POST")
+	router.HandleFunc(baseRoute+"/concepts/", ap.UpdateConcept).Methods("PUT")
+	router.HandleFunc(baseRoute+"/concepts/{conceptid}", ap.DeleteConcept).Methods("DELETE")
+
+	//Lesson routes
+	router.HandleFunc(baseRoute+"/lessons/{lessonid}", ap.GetLesson).Methods("GET")
+	router.HandleFunc(baseRoute+"/lessons/languages/{language}", ap.GetAllLessonsByLanguage).Methods("GET")
+	router.HandleFunc(baseRoute+"/lessons/", ap.CreateLesson).Methods("POST")
+	router.HandleFunc(baseRoute+"/lessons/", ap.UpdateLesson).Methods("PUT")
+	router.HandleFunc(baseRoute+"/lessons/{lessonid}", ap.DeleteLesson).Methods("DELETE")
+
+	//Course routes
+	router.HandleFunc(baseRoute+"/courses/{courseid}", ap.GetCourse).Methods("GET")
+	router.HandleFunc(baseRoute+"/courses/{courseid}/statistics", ap.GetCourseStatistics).Methods("GET")
+
+	//Questionary routes
+	//TODO
 }
 
 func (ap Application) signup(w http.ResponseWriter, r *http.Request) {
@@ -141,6 +161,7 @@ func (ap Application) protectedEndpoint(w http.ResponseWriter, r *http.Request) 
 
 }
 
+// GetConcept : Gets a concept by conceptID
 func (ap Application) GetConcept(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	var errorObject ahttp.Error
@@ -150,14 +171,24 @@ func (ap Application) GetConcept(w http.ResponseWriter, r *http.Request) {
 		ahttp.RespondWithError(w, http.StatusBadRequest, errorObject)
 		return
 	}
-	lang := vars["language"]
+
+	concept, err := ap.Client.GetConcept(r.Context(), id)
 	if err != nil {
-		errorObject.Message = "Invalid language"
-		ahttp.RespondWithError(w, http.StatusBadRequest, errorObject)
+		errorObject.Message = err.Error()
+		ahttp.RespondWithError(w, http.StatusInternalServerError, errorObject)
 		return
 	}
+	ahttp.RespondWithJSON(w, http.StatusOK, concept)
+}
 
-	concept, err := ap.Client.GetConcept(r.Context(), id, lang)
+// GetAllConcepts :
+func (ap Application) GetAllConceptsByLanguage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var errorObject ahttp.Error
+
+	lang := vars["language"]
+
+	concept, err := ap.Client.GetConceptsByLanguage(r.Context(), lang)
 	if err != nil {
 		errorObject.Message = err.Error()
 		ahttp.RespondWithError(w, http.StatusInternalServerError, errorObject)
@@ -179,6 +210,77 @@ func (ap Application) CreateConcept(w http.ResponseWriter, r *http.Request) {
 	ahttp.RespondWithJSON(w, http.StatusOK, concept)
 }
 
+// UpdateConcept :
+func (ap Application) UpdateConcept(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
+// DeleteConcept :
+func (ap Application) DeleteConcept(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
+// GetLesson : returns a lesson by lessonID
+func (ap Application) GetLesson(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var errorObject ahttp.Error
+	id, err := strconv.ParseInt(vars["lessonid"], 10, 0)
+	if err != nil {
+		errorObject.Message = "Invalid lesson ID"
+		ahttp.RespondWithError(w, http.StatusBadRequest, errorObject)
+		return
+	}
+
+	lesson, err := ap.Client.GetLesson(r.Context(), id)
+	if err != nil {
+		errorObject.Message = err.Error()
+		ahttp.RespondWithError(w, http.StatusInternalServerError, errorObject)
+		return
+	}
+	if lesson == nil {
+		errorObject.Message = "Lesson not found"
+		ahttp.RespondWithError(w, http.StatusInternalServerError, errorObject)
+		return
+	}
+	ahttp.RespondWithJSON(w, http.StatusOK, lesson)
+}
+
+// GetAllLessonsByLanguage : retrieves all lesson by language
+func (ap Application) GetAllLessonsByLanguage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var errorObject ahttp.Error
+
+	lang := vars["language"]
+
+	lessons, err := ap.Client.GetLessonsByLanguage(r.Context(), lang)
+	if err != nil {
+		errorObject.Message = err.Error()
+		ahttp.RespondWithError(w, http.StatusInternalServerError, errorObject)
+		return
+	}
+	if len(lessons) < 1 {
+		errorObject.Message = "Language " + lang + " has no lessons."
+		ahttp.RespondWithError(w, http.StatusInternalServerError, errorObject)
+		return
+	}
+	ahttp.RespondWithJSON(w, http.StatusOK, lessons)
+}
+
+// CreateLesson :
+func (ap Application) CreateLesson(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
+// UpdateLesson :
+func (ap Application) UpdateLesson(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
+// DeleteLesson :
+func (ap Application) DeleteLesson(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
 func TestEndpoint(w http.ResponseWriter, req *http.Request) {
 	token := context.Get(req, "user")
 	var user domain.User
@@ -191,6 +293,31 @@ func TestEndpoint(w http.ResponseWriter, req *http.Request) {
 		ahttp.RespondWithJSON(w, http.StatusOK, user)
 		//json.NewEncoder(w).Encode(user)
 	}
+}
+
+// GetCourse : returns a whole course
+func (ap Application) GetCourse(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	var errorObject ahttp.Error
+	id, err := strconv.ParseInt(vars["courseid"], 10, 0)
+	if err != nil {
+		errorObject.Message = "Invalid course ID"
+		ahttp.RespondWithError(w, http.StatusBadRequest, errorObject)
+		return
+	}
+
+	course, err := ap.Client.GetCourse(r.Context(), id)
+	if err != nil {
+		errorObject.Message = err.Error()
+		ahttp.RespondWithError(w, http.StatusInternalServerError, errorObject)
+		return
+	}
+	ahttp.RespondWithJSON(w, http.StatusOK, course)
+}
+
+// GetCourseStatistics :
+func (ap Application) GetCourseStatistics(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
--- domain/client.go
+++ domain/client.go
@@ -3,7 +3,9 @@ package domain
 import (
 	"fmt"
 	"log"
+	"os"
 
+	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql"
 	"github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
@@ -76,8 +78,22 @@ func (s *Client) Close() error {
 
 // Initialize : Initializes the mysql database connection
 func (s *Client) Initialize(user string, password string, ip string, port int, dbname string) {
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", user, password, ip, port, dbname)
-	// println("Connection: ", connectionString)
+	env := os.Getenv("ATUTOR_ENV")
+	connectionString := ""
+	if env == "test" {
+		connectionString = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", user, password, ip, port, dbname)
+	} else {
+		myInstanceName := "atutor:europe-west1:atutor-develop-db"
+		connectionString = fmt.Sprintf("%s:%s@cloudsql(%s)/%s?charset=utf8&parseTime=True&loc=UTC", user, password, myInstanceName, dbname)
+	}
+	//connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", user, password, ip, port, dbname)
+	//sql.Open("mysql", "cloudsql:my-instance*dbname/user/passwd")
+
+	// connectionStringDev := fmt.Sprintf("cloudsql:%s/%s/%s", myInstanceName, user, password)
+	// var dbConnectionString = fmt.Sprintf("%s:%s@cloudsql(atutor-develop-db)/%?charset=utf8&parseTime=True&loc=UTC", user, password
+	// var dbConnectionString = fmt.Sprintf("%s:%s@cloudsql(%s)/%s?charset=utf8&parseTime=True&loc=UTC", user, password, myInstanceName, dbname)
+	//user:password@cloudsql(copiedPastedInstanceConnectionName)/databaseName?charset=charset&collation=collation&tls=tlsConfigName&parseTime=true
+	println("Connection: ", connectionString)
 	var err error
 	s.db, err = gorm.Open("mysql", connectionString)
 	if err != nil {
@@ -85,4 +101,5 @@ func (s *Client) Initialize(user string, password string, ip string, port int, d
 	} else {
 		fmt.Println("Connection Established.")
 	}
+
 }
